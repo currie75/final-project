54b0eba345f0ff72314f624c43f441b590796ab9
currie75 <71086382+currie75@users.noreply.github.com>
2022-01-12 15:29:00 +0000

    tested and added guessing system and check to see if my hangman chart thing would work.

--- menu.py
+++ menu.py
@@ -71,7 +71,20 @@ Max_wrong = len(Hangman) - 1
 word = random.choice(word_list)
 #
 current_guess = "-" * len(word)
+#
+wrong_guess = 0
+#
+used_letters = []
 
+#
 print("\033[1;34;20m \n")
-print(word)
-print(current_guess)
+print("This is Hangman")
+print("lets play")
+#
+while wrong_guess < Max_wrong and current_guess != word:
+    print(Hangman[wrong_guess])
+    print("you have used these following letter: ",used_letters)
+    print("your current word is so far: ",current_guess)
+#
+guess = input("enter letters here:")
+guess = guess.upper()
