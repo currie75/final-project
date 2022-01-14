fdf79cb71a617372cd186c735279f721151e8283
currie75 <71086382+currie75@users.noreply.github.com>
2022-01-14 15:31:08 +0000

    did some test runs and code isn't up to par yet

--- menu.py
+++ menu.py
@@ -77,10 +77,11 @@ wrong_guess = 0
 used_letters = []
 
 #
-print("\033[1;34;20m \n")
+print("\033[1;35;20m \n")
 print("This is Hangman")
 print("lets play")
 #
+
 while wrong_guess < Max_wrong and current_guess != word:
     print(Hangman[wrong_guess])
     print("you have used these following letter: ",used_letters)
@@ -88,3 +89,32 @@ while wrong_guess < Max_wrong and current_guess != word:
 #
 guess = input("enter letters here:")
 guess = guess.upper()
+
+while guess in used_letters:
+    print("You have already used that letter", guess)
+    guess = input("add your letters to guess:")
+    guess = guess.upper()
+
+used_letters.append(guess)
+
+if guess in word:
+    print("you have correctly guess a letter!")
+
+    new_current_guess = ""
+    for letters in range(len(word)):
+        if guess == word[letters]:
+            new_current_guess += guess
+        else:
+            new_current_guess += current_guess[letters]
+
+    current_guess = new_current_guess
+else:
+    print("sorry thats a wrong guess")
+
+    wrong_guess +=1
+
+
+if wrong_guess == Max_wrong:
+    print(Hangman[current_guess])
+    print("you got hanged up mann!")
+    print("the correct word is", word)
